c7c75d5e94e9e38959b0f14531644621cd1ed1e4
pepsiplusmilk <lol.fleeeex@gmail.com>
2025-10-08 12:54:42 +0300

    feat(tests): add nfr coverage

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -32,21 +32,6 @@ def test_board_for_new_idea_not_found():
     assert response.status_code == 422
 
 
-def test_board_ended_for_new_idea():
-    response = client.post("/new_board?new_board_topic=new_board")
-    assert response.status_code == 200
-
-    board_id = response.json().get("board_id")
-    print(board_id)
-    response = client.patch(f"/vote_boards/{board_id}/close")
-    assert response.status_code == 200
-
-    response = client.post(f"/vote_boards/{board_id}/new_idea?idea_title=new&idea_desc=none")
-    assert response.status_code == 400
-    body = response.json()
-    assert "error" in body and body["error"]["code"] == "bad_request"
-
-
 def test_single_idea_get_not_found():
     rand_uuid = uuid.uuid4()
     response = client.get(f"/ideas/{str(rand_uuid)}")
